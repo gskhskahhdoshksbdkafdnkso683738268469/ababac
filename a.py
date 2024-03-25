913ea56233f2d6de969877cf03da8856dd8c8586
gskhskahhdoshksbdkafdnkso683738268469 <164631811+gskhskahhdoshksbdkafdnkso683738268469@users.noreply.github.com>
2024-03-25 15:19:35 +0600

    Create a.py

---
+++ a.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import requests as r
+
+app = Flask(__name__)
+
+@app.route("/<path:b>")
+def aa(b):
+    x = r.get("https://prod-ec-us-west-2.video.pscp.tv/"+b)
+    h = {"Content-Type":x.headers["Content-Type"],"Access-Control-Allow-Origin":"*"}
+    return x.content,200,h
+
+app.run(host="0.0.0.0")
