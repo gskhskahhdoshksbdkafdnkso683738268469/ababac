9ab066acdfc9a96b9e1c83a3905770f8cde3b878
gskhskahhdoshksbdkafdnkso683738268469 <164631811+gskhskahhdoshksbdkafdnkso683738268469@users.noreply.github.com>
2024-03-25 16:01:30 +0600

    Update a.py

--- a.py
+++ a.py
@@ -9,4 +9,10 @@ def aa(b):
     h = {"Content-Type":x.headers["Content-Type"],"Access-Control-Allow-Origin":"*"}
     return x.content,200,h
 
+@app.route("/ab")
+def bb():
+    x = r.get("http://51.159.98.123:80/live/415663775/826262627/109987.ts?token=GhMNBEZcR1gRUARbB18BBAIOVlVYVQsHVlZUAFBeBlUEVVUGBFVXVANEGBoXEhZRBVtrXgVGAgFSXw4MBBQUFhJUFzpcUEdYEVADC1xQUhZIFEBeDAEaClNRBgUADgZWVAFJR0VdAUAJRAwIU1ZGGEZTTEMEFlZRDjlTUUVRVQZDCwsQWVhJQFwIa1wBCwVaABQOFVBGFBILFUcWCVZDDw0dRwdcQBcDRwMWAkZXUwxXFBgVAgtNXhYUThYJGnQnQx1HAE1AAAxAD1tWRlxGQBcUGBUIF2dCBxVDRlZZWwYTE19HBRZJQF4HTGcHCQpaAVVAXg4KSxJYRAYWHxpbDA9YEQpHaxULV0QOGlxWUgJGSw==")
+    h = {"Content-Type":x.headers["Content-Type"],"Access-Control-Allow-Origin":"*"}
+    return x.content,200,h
+
 app.run(host="0.0.0.0")
